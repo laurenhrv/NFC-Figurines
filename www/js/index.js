2737e904549c89690d724e0803926e8954253f1b
laurenhrv <laurenhr-v@hotmail.com>
2017-03-29 10:02:35 -0400

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,111 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var savePage = false;
+var x = "data";
+var phone = document.getElementById("phone");
+var figurine = document.getElementById("figurine");
+
+save.addEventListener("click", function() {
+    savePage = true;
+}
+
+scan.addEventListener("click", function() {
+    scanData();
+})
+
+log.addEventListener("click", function() {
+    console.log(x);
+    console.log(savePage);
+})
+
+change.addEventListener("click", function() {
+    changeStatus();
+    phone.innerHTML = x;
+})
+
+function changeStatus() {
+    if (x =="data") {
+        x ="empty";
+    } else if (x =="empty") {
+        x = "data";
+    }
+}
+
+function scanData() {
+    if (savePage == false) {
+        x = figurine.innerHTML;
+        phone.innerHTML = x;
+    } else if (savePage == true) {
+        figurine.innerHTML = x;
+        savePage = false;
+    }
+}
+
+
+//////////////////////////////
+
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        nfc.addNdefListener (
+            function (nfcEvent) {
+                var tag = nfcEvent.tag,
+                ndefMessage = tag.ndefMessage;
+
+                // alert(JSON.stringify(ndefMessage));
+                scanData();
+                // alert(nfc.bytesToString(ndefMessage[0].payload).substring(0));
+            }, 
+            function () { // success callback
+                alert("Waiting for NDEF tag");
+            },
+            function (error) { // error callback
+                alert("Error adding NDEF listener " + JSON.stringify(error));
+            }
+        );
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
